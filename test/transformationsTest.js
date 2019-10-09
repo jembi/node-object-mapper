de7e4435de8ac8d712c69cee449dce5e9a0a0efc
Brent Engelbrecht <brent.engelbrecht@jembi.org>
2019-10-09 16:59:27 +0200

    Add happy case test for transform dateTimeToDate OHM-871

---
+++ test/transformationsTest.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const test = require('tape')
+const transformations = require('../src/transformations')
+
+test.test('Transformations', t => {
+  t.test('dateTimeToDate()', t => {
+    t.test('should convert iso datetime string to date', t => {
+      const inputDate = '1988-03-24T00:00:00.000+0800'
+      const expected = '1988-03-24'
+
+      const result = transformations.dateTimeToDate(inputDate)
+
+      t.equal(result, expected)
+      t.end()
+    })      
+  })
+
+  t.test('createBahmniFhirIdentifier()', t => {
+    t.test('should convert iso datetime string to date', t => {
+      const inputDate = '1988-03-24T00:00:00.000+0800'
+      const expected = '1988-03-24'
+
+      const result = transformations.dateTimeToDate(inputDate)
+
+      t.equal(result, expected)
+      t.end()
+    })      
+  })
+})
