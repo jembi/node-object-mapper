8c4fad8dab1ac0997ca0931c7eb650c1dde67983
MattyJ007 <matthew.dickie@jembi.org>
2019-10-10 15:46:48 +0200

    Add tests for gender code map function
    Test it works as expected
    
    OHM-871

--- test/transformationsTest.js
+++ test/transformationsTest.js
@@ -16,6 +16,38 @@ test.test('Transformations', t => {
     })
   })
 
+  t.test('getGenderFromCode()', t => {
+    t.test('should convert single letter code to word', t => {
+      const inputCode = 'F'
+      const expectedOutput = 'female'
+
+      const result = transforms['getGenderFromCode'](inputCode)
+
+      t.equal(result, expectedOutput)
+      t.end()
+    })
+
+    t.test('should convert unhandled gender codes to unknown', t => {
+      const inputCode = 7
+      const expectedOutput = 'unknown'
+
+      const result = transforms['getGenderFromCode'](inputCode)
+
+      t.equal(result, expectedOutput)
+      t.end()
+    })
+
+    t.test('should convert null gender codes to unknown', t => {
+      const inputCode = null
+      const expectedOutput = 'unknown'
+
+      const result = transforms['getGenderFromCode'](inputCode)
+
+      t.equal(result, expectedOutput)
+      t.end()
+    })
+  })
+
   t.test('booleanFlip()', t => {
     t.test('should convert true to false', t => {
       const booleanInput = true
