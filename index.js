cd105978dca7bf4c9f02e0243936bc3bbebe5245
MattyJ007 <matthew.dickie@jembi.org>
2019-10-10 15:21:51 +0200

    Refactor project to reflect Jembi's ownership
    The project has been modified from its fork origin therefore these
    chagnes need to be represented.
    
    All references to previous owner in the code in liceses and contacts
    will be removed.
    
    New code features and how to guides will be updated
    
    Reference new repo location
    
    OHM-871

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  The MIT License (MIT)
  =====================
 
- Copyright (c) 2012 Daniel L. VerWeire
+ Copyright (c) 2019 Jembi Health Systems NPC
 
  Permission is hereby granted, free of charge, to any person obtaining
  a copy of this software and associated documentation files (the
