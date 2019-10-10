6070e0ca5476a3af7418f6e583d7e1442b6eafac
MattyJ007 <matthew.dickie@jembi.org>
2019-10-10 14:29:44 +0200

    Remove duplicate transformation test
    Datetime to date test is alredy there
    
    OHM-871

--- test/transformationsTest.js
+++ test/transformationsTest.js
@@ -13,18 +13,6 @@ test.test('Transformations', t => {
 
       t.equal(result, expected)
       t.end()
-    })      
-  })
-
-  t.test('createBahmniFhirIdentifier()', t => {
-    t.test('should convert iso datetime string to date', t => {
-      const inputDate = '1988-03-24T00:00:00.000+0800'
-      const expected = '1988-03-24'
-
-      const result = transformations.dateTimeToDate(inputDate)
-
-      t.equal(result, expected)
-      t.end()
-    })      
+    })
   })
 })
