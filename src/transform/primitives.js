af21cfcaa1ce88faf9803ebd58a05dfa0ba02347
MattyJ007 <matthew.dickie@jembi.org>
2019-10-11 11:52:08 +0200

    Add function to convert user values to boleans
    Generally a useful mapping functionality
    
    OHM-871

--- src/transform/primitives.js
+++ src/transform/primitives.js
@@ -10,5 +10,19 @@ exports.primitives = {
     }
 
     return null
+  },
+  booleanify: function(val) {
+    if (val === 'false') {
+      return false
+    }
+
+    // null or undefined values will return null
+    if (val == null) {
+      return null
+    }
+
+    // all other values will be converted to boolean by their truthy or falsy value
+    // ie: 0, 0n, '', ``, "", NaN, false are falsy and everything else including -1, {}, [] are truthy
+    return !!val
   }
 }
